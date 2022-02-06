f4086c0aa06ae77b423a9ffa925b8801b51a5e32
TenzKD <kungadamchoe@hotmail.com>
2022-02-06 21:59:15 +0100

    Challenge solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...num) {
+  for (let i = 0; i <= arr.length; i++) {
+    for (let j = 0; j <= num.length; j++) {
+      if (arr[i] === num[j] && arr[i] !== null && arr[i]) {
+        arr.splice(i, 1);
+        i = 0;
+        j = 0;
+      } else {
+        continue;
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
