e479e4a394ba35695cc78b8060b332a4202c846a
TenzKD <kungadamchoe@hotmail.com>
2022-02-07 14:25:36 +0100

    Challenge solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,10 @@
-const ftoc = function (temp) {
-  let cels = (temp - 32) * (5 / 9);
+const ftoc = function (f) {
+  let cels = (f - 32) * (5 / 9);
   return Math.round(cels * 10) / 10;
 };
 
-const ctof = function (temp) {
-  let fahr = temp * (9 / 5) + 32;
+const ctof = function (c) {
+  let fahr = c * (9 / 5) + 32;
   return Math.round(fahr * 10) / 10;
 };
 
