1f3a367d18d7860c4fdb77c2ec6bfae34c00f6d5
TenzKD <kungadamchoe@hotmail.com>
2022-03-13 20:27:47 +0100

    Challenge solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  /* if (arr == "") {
+    return 0;
+  } else if (arr.length === 1) {
+    return arr[0];
+  } else {
+    return arr.reduce((a, b) => {
+      return a + b;
+    });
+  } */
+  return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((a, b) => {
+    return a * b;
+  });
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let factor = 1;
+  if (a === 0) {
+    return 1;
+  } else {
+    for (let i = 1; i <= a; i++) {
+      factor *= i;
+    }
+    return factor;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +48,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
