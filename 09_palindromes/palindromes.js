2e08d74ab613ccfddfe025613a8b40cd9e856873
TenzKD <kungadamchoe@hotmail.com>
2022-03-14 21:43:45 +0100

    Challenge solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ";
+  let cleanStr = str
+    .split("")
+    .filter((letter) => {
+      return punctuation.indexOf(letter) === -1;
+    })
+    .join("");
 
+  let newStr = cleanStr.split("").reverse().join("");
+  if (cleanStr.toLowerCase() === newStr.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
