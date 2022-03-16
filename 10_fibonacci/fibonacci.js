f17f17f8e64bfe890e8f1d97e75d42aa1846adc1
TenzKD <kungadamchoe@hotmail.com>
2022-03-16 22:09:05 +0100

    Challenge solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (numb) {
+  if (numb < 0) {
+    return "OOPS";
+  } else {
+    let fibo = [1, 1];
+    for (let i = 2; i < numb; i++) {
+      fibo.push(fibo[i - 2] + fibo[i - 1]);
+    }
+    return fibo[numb - 1];
+  }
 };
 
 // Do not edit below this line
