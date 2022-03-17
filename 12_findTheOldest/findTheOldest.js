a6398aedfa0b5920ee65c66a5e8b6b91e6e70cf9
TenzKD <kungadamchoe@hotmail.com>
2022-03-17 22:53:43 +0100

    Challenge solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+/* const findTheOldest = function (oldest) {
+  return oldest.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    return oldestAge < currentAge ? current : oldest;
+  });
+};
+function getAge(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+} };*/
 
+const findTheOldest = function (people) {
+  people.map((person) => {
+    if (typeof person.yearOfDeath === "undefined") {
+      return (person.yearOfDeath = new Date().getFullYear());
+    }
+  });
+  const oldest = people.sort((first, sec) => {
+    const a = first.yearOfDeath - first.yearOfBirth;
+    const b = sec.yearOfDeath - sec.yearOfBirth;
+    return a > b ? -1 : 1;
+  })[0];
+  return oldest;
 };
 
 // Do not edit below this line
